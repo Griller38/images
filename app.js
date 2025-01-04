e1e8fb0645e94083dcdc337e519d1dd76598f404
Grillvelg <ddirkx38@gmail.com>
2025-01-04 16:30:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('#mobile-menu')
+const menulinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menulinks.classList.toggle('active');
+} )
+
+document.getElementById('scrollToServices').addEventListener('click', function() {
+    document.getElementById('services').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
